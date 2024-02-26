4374c218d4f1825063431bd24843766cc799cfed
Aswin Lal M <aswnss.m@gmail.com>
2024-02-26 08:50:56 +0530

    blueprint animation component
    created a new reusable component for the blueprint advent logo , tried path animations but the svg is not stroke based so switched to simple opacity fade in

---
+++ pages/test/index.tsx
@@ -0,0 +1,9 @@
+import BlurPrintAnimation from "@/components/ui/BluePrintAnimation";
+
+export default function TestPage() {
+    return (
+        <div className={` w-screen min-h-screen flex items-center justify-center `}>
+            <BlurPrintAnimation />
+        </div>
+    )
+}
