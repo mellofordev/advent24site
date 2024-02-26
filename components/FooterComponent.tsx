9c036a4f127cd14fae95df4ac24fcd34f2379e41
sreedhar <sreedharks2003@gmail.com>
2024-02-26 06:40:43 +0530

    from bash

---
+++ components/FooterComponent.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function Footer() {
+    return (
+        <div className="flex flex-col justify-items-center mb-20 text-center">
+            <p className="text-slate-300">advent24</p>
+        </div>
+    );
+}
