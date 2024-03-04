90c4f73c656077097cc55f127c8c4f0b745b4dc9
sreedhar <sreedharks2003@gmail.com>
2024-03-04 21:11:46 +0530

    feat : added Fibonacci pattern fix: improved home ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,24 +9,17 @@ import AdventBlueprint from "@/components/svg/AdventBlueprint";
 import Ballon from "@/components/svg/Ballon";
 import { Button } from "@/components/ui/button";
 import LightHouse from "@/components/svg/LightHouse";
+import FibonacciPattern from "@/components/svg/FibonacciPattern";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="main">
+    <div className="flex flex-col justify-items-center bg-[#FFE0B3]">
       <Head>
         <title>advent'24</title>
       </Head>
       <div className="flex flex-col justify-items-center relative overflow-x-hidden">
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ ease: "easeIn", duration: 1 }}
-          className="absolute bottom-2.5/4 right-1/2 z-[100]"
-        >
-          <LightHouse className="w-30" />
-        </motion.div>
         <motion.div
           className={styles.largeViewer}
           initial={{ opacity: 0, transform: "translateZ(-120px)" }}
@@ -54,19 +47,50 @@ export default function Home() {
             whileInView={{ opacity: 1, transform: "translateY(0px)" }}
             transition={{ ease: "linear", duration: 2 }}
           />
+          <motion.div
+            className="absolute flex flex-row justify-evently gap-5 top-2/3 left-24"
+            initial={{ opacity: 0, transform: "translateY(50px)" }}
+            whileInView={{ opacity: 1, transform: "translateY(0px)" }}
+            transition={{ ease: "linear", duration: 2 }}
+          >
+            <Button>brochure</Button>
+            <Button>caw'24</Button>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ ease: "easeIn", duration: 1 }}
+            className="absolute top-3/4 left-1/2"
+          >
+            <Ballon className="w-30" />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ ease: "easeIn", duration: 1 }}
+            className="absolute top-0 right-1/2 z-[100]"
+          >
+            <LightHouse className="w-30" />
+          </motion.div>
         </motion.div>
-        <div className="absolute flex flex-row justify-evently gap-5 top-2/3 left-24">
-          <Button>events</Button>
-          <Button>caw24</Button>
+        <div className="relative flex flex-col items-center p-6 h-full text-center bg-[url(../public/bg_paper.jpg)] bg-[#9f7b55] bg-blend-luminosity">
+          <p className="text-[14px] italic font-[600] uppercase ">
+            The much awaited event will start in
+          </p>
+          <FibonacciPattern className="m-5"/>
+          <div className="absolute top-2/4">
+            <div className="flex flex-row justify-evently gap-3">
+              <h1 className="text-[57px] text-[#562C02] font-[400]">00</h1>
+              <h1 className="text-[57px] text-[#562C02] font-[400]">00</h1>
+              <h1 className="text-[57px] text-[#562C02] font-[400]">00</h1>
+              <h1 className="text-[57px] text-[#562C02] font-[400]">00</h1>
+            </div>
+          </div>
+          <div className="absolute top-3/4 flex flex-col justify-center">
+            <p>venue:</p>
+            <h1 className="text-[#562C02] uppercase text-[14px] font-[400]">Sree Chitra Thirunal College of Engineering</h1>
+          </div>
         </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ ease: "easeIn", duration: 1 }}
-          className="absolute top-3/4 left-1/2"
-        >
-          <Ballon className="w-30" />
-        </motion.div>
       </div>
       <Footer />
     </div>
